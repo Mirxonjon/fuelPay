759acafb7062098cadb0514399ab8dea0c11bee5
Mirxonjon <mirxonjonismanov152@gmail.com>
2026-03-31 20:33:45 +0500

    fixed bug

--- src/modules/fuel-station/fuel-station.service.ts
+++ src/modules/fuel-station/fuel-station.service.ts
@@ -117,6 +117,7 @@ export class FuelStationService {
                     picture: true,
                     octane: true,
                     unit: true,
+                    category:true
                   },
                 },
               },
